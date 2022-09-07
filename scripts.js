57f845e747a4c974982c935bfa7d7c58d8fbede5
Fania Everitt <me@fania.eu>
2022-09-07 16:49:35 +0100

    still locking stuff, or not

--- scripts.js
+++ scripts.js
@@ -88,22 +88,18 @@ resetButton.addEventListener('click', ()=> {
 
 const triggers = document.getElementsByTagName('figure');
 [...triggers].forEach( trig => {
-  trig.addEventListener('click', ()=> { 
-    console.log(trig);
+  trig.addEventListener('click', ()=> {
+    console.log("=========================="); 
+    console.log("click trigger:",trig);
 
     const skill = trig.lastElementChild.firstElementChild.innerHTML;
     localStorage.setItem(`aragami2-total`, parseInt(skillpoints));
 
-    // FIRST CLICK (UNLOCKED)
+    // FIRST CLICK (UNLOCKED -> SELECTED)
     if(trig.classList.length == 0){
       // change image
       trig.classList.add("first");
-      const old = trig.children[0].attributes.src.nodeValue;
-      const neww = old.replace("-locked","-selected");
-      console.log(trig.children[0]);
-      console.log(neww);
-      trig.children[0].setAttribute("src",neww);
-      // replaceImgs("select",neww);
+      replaceImgs("select",trig);
       // add points to storage
       localStorage.setItem(`aragami2-${skill}`, costs[skill][0]);
       // add points to totals
@@ -111,14 +107,11 @@ const triggers = document.getElementsByTagName('figure');
       localStorage.setItem(`aragami2-total`, parseInt(skillpoints));
       num.innerHTML = localStorage.getItem("aragami2-total");
     }
-    // SECOND CLICK (UPGRADED)
+    // SECOND CLICK (SELECTED -> UPGRADED)
     else if(trig.classList.contains("first")){
       // change image
       trig.classList.replace("first","second");
-      const old = trig.children[0].attributes.src.nodeValue;
-      const neww = old.replace("-selected","-upgraded");
-      trig.children[0].setAttribute("src",neww);
-      // replaceImgs("upgrade",neww);
+      replaceImgs("upgrade",trig);
       // add points to storage
       localStorage.setItem(`aragami2-up-${skill}`, costs[skill][1]);
       // add points to totals
@@ -126,20 +119,11 @@ const triggers = document.getElementsByTagName('figure');
       localStorage.setItem(`aragami2-total`, parseInt(skillpoints));
       num.innerHTML = localStorage.getItem("aragami2-total");
     } 
-    // RESET CLICK
+    // RESET CLICK (ANY -> LOCKED)
     else if(trig.classList.contains("second")){
       // change image
       trig.classList.remove("second");
-      const old = trig.children[0].attributes.src.nodeValue;
-      let neww = "";
-      if(old.endsWith("-selected")) {
-        neww = old.replace("-selected","-locked");
-        // replaceImgs("lock",neww);
-      } else {
-        neww = old.replace("-upgraded","-locked");
-        // replaceImgs("lock",neww);
-      }
-      trig.children[0].setAttribute("src",neww);
+      replaceImgs("lock",trig);
       // remove points from storage
       localStorage.removeItem(`aragami2-${skill}`);
       localStorage.removeItem(`aragami2-up-${skill}`);
@@ -150,44 +134,52 @@ const triggers = document.getElementsByTagName('figure');
       num.innerHTML = localStorage.getItem("aragami2-total");
     }
 
+    // DEFAULT STATE (LOCKED -> UNLOCKED)
     // DISABLE ROWS if necessary on each click
+    // check logic here?????????
     let currentTotal = localStorage.getItem(`aragami2-total`);
     // console.log(currentTotal, typeof currentTotal);
     if( currentTotal == 30) {
       disableRows(rows);
+      rows.forEach(r => lockRow(r));
       currentTotal = parseInt(localStorage.getItem(`aragami2-total`));
       console.log("equal 30", currentTotal);
     } 
     // minimum 29
     if( currentTotal < 30 && currentTotal >= 28) {
       enableRow(row2);
+      unlockRow(row2);
       currentTotal = parseInt(localStorage.getItem(`aragami2-total`));
-      console.log("less 30, greater 28", currentTotal);
+      console.log("less 30, greater= 28", currentTotal);
     }
     // minimum 25
     if( currentTotal < 28 && currentTotal >= 25) {
       enableRows([row2,row3]);
+      [row2,row3].forEach(r => unlockRow(r));
       currentTotal = parseInt(localStorage.getItem(`aragami2-total`));
-      console.log("less 28, greater 25", currentTotal);
+      console.log("less 28, greater= 25", currentTotal);
     }
     // minimum 21
     if( currentTotal < 25 && currentTotal >= 21) {
       enableRows([row2,row3,row4]);
+      [row2,row3,row4].forEach(r => unlockRow(r));
       currentTotal = parseInt(localStorage.getItem(`aragami2-total`));
-      console.log("less 25, greater 21", currentTotal);
+      console.log("less 25, greater= 21", currentTotal);
     }
     // minimum 17
     if( currentTotal < 21 && currentTotal >= 17) {
       enableRows([row2,row3,row4,row5]);
+      [row2,row3,row4,row5].forEach(r => unlockRow(r));
       currentTotal = parseInt(localStorage.getItem(`aragami2-total`));
-      console.log("less 21, greater 17", currentTotal);
+      console.log("less 21, greater= 17", currentTotal);
     }
     // minimum 13
     // if( currentTotal < 17 && currentTotal >= 13) {
     if( currentTotal < 17) {
-      enableRows([row2,row3,row4,row5]);
+      enableRows(rows);
+      rows.forEach(r => unlockRow(r));
       currentTotal = parseInt(localStorage.getItem(`aragami2-total`));
-      console.log("less 17, greater 13", currentTotal);
+      console.log("less 17", currentTotal);
     }
   });
 });
@@ -233,38 +225,68 @@ function loadFromStorage() {
 
 
 function replaceImgs(status, item) {
+  console.log("replacing imgs",status,item);
   const old = item.children[0].attributes.src.nodeValue;
+  console.log("old",old);
   let neww = "";
-  if(status === "unlock") {
+  if(status == "unlock") {
     neww = old.replace("-locked","-unlocked");
   } 
-  if(status === "select"){
-    neww = old.replace("-unlocked","-selected");
+  if(status == "select"){
+    if(old.includes("-locked")) {
+      neww = old.replace("-locked","-selected");
+    } else {
+      neww = old.replace("-unlocked","-selected");
+    }
   }
-  if(status === "upgrade"){
+  if(status == "upgrade"){
     neww = old.replace("-selected","-upgraded");
   }
-  if(status === "lock"){
-    neww = old.replace("-upgraded","-locked");
+  if(status == "lock"){
+    if(old.includes("-selected")) {
+      neww = old.replace("-selected","-locked");
+    } else if(old.includes("-unlocked")) {
+      neww = old.replace("-unlocked","-locked");
+    } else {
+      neww = old.replace("-upgraded","-locked");
+    }
   }
+  console.log("neww",neww);
   // console.log(item.children[0].classList);
   item.children[0].setAttribute("src",neww);
 }
 
 
+// LOCKED -> UNLOCKED
+function unlockRow(row) {
+  console.log("unlocking",row);
+  for (let r of row.children) {
+    replaceImgs("unlock", r);
+  }
+}
+// UNLOCKED -> LOCKED
+function lockRow(row) {
+  console.log("locking",row);
+  for (let r of row.children) {
+    replaceImgs("lock", r);
+  }
+}
 
 function disableRow(row) {
-  console.log("disabling", row);
   row.classList.add("disable");
 }
 function enableRow(row) {
-  console.log("enabling", row);
   row.classList.remove("disable");
 }
 function disableRows(rows) {
-  rows.forEach(r => disableRow(r));
+  console.log("disabling", rows);
+  rows.forEach(r => {
+    
+    disableRow(r)
+  });
 }
 function enableRows(rows) {
+  console.log("enabling", rows);
   rows.forEach(r => enableRow(r));
 }
 
